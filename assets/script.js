6b6b3db610b83073faab580fd4f2411633892a06
PDKetchum <pxiong265@gmail.com>
2022-08-11 00:08:00 -0500

    added api fetch

--- assets/script.js
+++ assets/script.js
@@ -6,11 +6,32 @@ var $searchButton = $("#search-button");
 
 var dateUnix = moment().unix();
 var date = moment().format("MMMM Do, YYYY");
-var lat = 44.970797;
-var lon = -93.3315183;
+var lat;
+var lon;
+var temp;
+var wind;
+var humidity;
 console.log($searchButton);
 
-function search(query, format) {
+function searchCoordinates() {
+  var city = $searchInput.val();
+  var url =
+    "https://api.openweathermap.org/geo/1.0/direct?q=$" +
+    city +
+    "&limit=1&appid=56d93d0b8d7b2c21a7f8fd90a7c509fc";
+  fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      lon = data[0].lon;
+      lat = data[0].lat;
+      searchWeather();
+    });
+}
+
+function searchWeather() {
   var url =
     "https://api.openweathermap.org/data/2.5/onecall?lat=" +
     lat +
@@ -26,14 +47,24 @@ function search(query, format) {
     })
     .then(function (data) {
       console.log(data);
+
+      temp = data.current.temp;
+      wind = data.current.wind_speed;
+      humidity = data.current.humidity;
+      $("#today-temp").text("Today's temperature: " + temp);
     });
 }
-
-search();
 
 $searchButton.on("click", print);
 
 function print() {
-  $todayDate.text(date);
-  $cityName.text($searchInput.val());
+  printToday();
+  searchCoordinates();
 }
+
+function printToday() {
+  $todayDate.text(date);
+}
+
+// "lat": 44.9772995,
+// "lon": -93.2654692,
